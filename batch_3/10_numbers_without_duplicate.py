3304071b632b8829fe47857791d1d86856bdb336
AzarconAD <adamdaniel2801@gmai.com>
2025-03-12 12:38:51 +0800

    batch 3 program 2 done

--- batch_3/10_numbers_without_duplicate.py
+++ batch_3/10_numbers_without_duplicate.py
@@ -3,6 +3,7 @@ print("Please enter 10 numbers")
 num_list = []
 for i in range (1,11):
     num = int(input(f"Number {i}: "))
-    num_list.append(num)
+    if num not in num_list:
+        num_list.append(num)
 
 print(num_list)
