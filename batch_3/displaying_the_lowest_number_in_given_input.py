695070d0b9f3bca9e39f0849b3af45ba31ccede6
AzarconAD <adamdaniel2801@gmai.com>
2025-03-13 10:52:24 +0800

    batch 3 program 4 done

---
+++ batch_3/displaying_the_lowest_number_in_given_input.py
@@ -0,0 +1,14 @@
+num_list = []
+
+
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        num_list.append(num)
+
+        sorted_list = sorted(num_list)
+        print(sorted_list[0])
+    except:
+        ValueError
+        break
+
