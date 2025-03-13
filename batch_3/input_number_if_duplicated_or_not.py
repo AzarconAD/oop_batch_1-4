603cb3a407bf38a0e465274eb8546083dbd431e1
AzarconAD <adamdaniel2801@gmai.com>
2025-03-13 09:51:45 +0800

    batch 3 program 3 2nd part successful

--- batch_3/input_number_if_duplicated_or_not.py
+++ batch_3/input_number_if_duplicated_or_not.py
@@ -1,6 +1,15 @@
 while True:
     try: 
-        entered_number = int(input("Input a number: "))
+        num_list = [int(input("Input a number: "))]
+
+        unique_numbers = []
+        for num in num_list:
+            if num_list.count(num) == 1:
+                print("Unique")
+                unique_numbers.append(num)
+                if num_list.count(num) != 1:
+                    print("Duplicate")
+
     except:
         ValueError
         print("invalid input!")
